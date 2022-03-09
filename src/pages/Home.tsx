7cfd8a4dc55ab9eaeee35596b4efa3d15722dba4
wagnercc <wagcamargocastilho@gmail.com>
2022-03-08 22:34:15 -0300

    add functionally to add, mark as check and remove todo list

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,30 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    console.log("test", newTaskTitle)
+    const data: Task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    };
+
+    setTasks(oldTasks => [...oldTasks, data]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const tasksFiltered = tasks.map(task => ({ ...task }));
+    const taskToCheck = tasksFiltered.find(task => task.id === id);
+
+    if(!taskToCheck)
+      return;
+
+    taskToCheck.done = !taskToCheck.done;
+
+    setTasks(tasksFiltered);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(tasks => tasks.filter(task => task.id !== id));
   }
 
   return (
